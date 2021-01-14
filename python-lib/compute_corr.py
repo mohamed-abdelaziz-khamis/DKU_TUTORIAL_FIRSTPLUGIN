2345830d17fd050d90ff6fdd52783b021b00b30a
momar <momar@infme.com>
2021-01-14 10:46:51 +0200

    Edited file 'python-lib/compute_corr.py'

--- python-lib/compute_corr.py
+++ python-lib/compute_corr.py
@@ -1,13 +1,14 @@
-# We'll only compute correlations on numerical columns
-# So extract all pairs of names of numerical columns
-pairs = []
-for i in range(0, len(column_names)):
-    for j in range(i + 1, len(column_names)):
-        col1 = column_names[i]
-        col2 = column_names[j]
-        if df[col1].dtype == "float64" and \
-           df[col2].dtype == "float64":
-            pairs.append((col1, col2))
+def compute_pairs()
+    # We'll only compute correlations on numerical columns
+    # So extract all pairs of names of numerical columns
+    pairs = []
+    for i in range(0, len(column_names)):
+        for j in range(i + 1, len(column_names)):
+            col1 = column_names[i]
+            col2 = column_names[j]
+            if df[col1].dtype == "float64" and \
+               df[col2].dtype == "float64":
+                pairs.append((col1, col2))
 
 # Compute the correlation for each pair, and write a
 # row in the output array
